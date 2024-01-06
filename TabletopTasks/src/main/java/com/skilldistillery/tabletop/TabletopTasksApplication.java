f2a9b5560d2a5fb9361e0cb670c26374eff0d286
VulcanMindForge <jastuart@live.com>
2024-01-05 17:25:33 -0700

    Added Spring starter and finished set up

---
+++ TabletopTasks/src/main/java/com/skilldistillery/tabletop/TabletopTasksApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.tabletop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabletopTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabletopTasksApplication.class, args);
+	}
+
+}
---
+++ TabletopTasks/src/test/java/com/skilldistillery/tabletop/TabletopTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.tabletop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabletopTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
